ff9e76e8a2561a16dc2d968c39a2f4eea5b09c86
ethanwhitcher <ewhitcher123@gmail.com>
2023-05-03 22:15:22 -0400

    Initial commit

---
+++ src/main/java/io/springbootstart/CourseRestAPI/CourseRestApiApplication.java
@@ -0,0 +1,13 @@
+package io.springbootstart.CourseRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/springbootstart/CourseRestAPI/CourseRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbootstart.CourseRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
